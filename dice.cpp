d56cda11ae0c2fca6660c7005a06e51409a3f075
Anton Pavlyukevich <69690074+itonyluke@users.noreply.github.com>
2022-01-08 17:24:08 +0300

    Update dice.cpp

--- dice.cpp
+++ dice.cpp
@@ -1,3 +1,24 @@
+//Copyright 2022 Anton Pavlyukevich.
+
+//dice is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//dice is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with dice.  If not, see <https://www.gnu.org/licenses/>.
+
+//
+// Created by pavlyukevich.anton@gmail.com on 08.01.22
+//
+
+// built and tested on Darwin Kernel Version 18.7.0: Tue Jun 22 19:37:08 PDT 2021; root:xnu-4903.278.70~1/RELEASE_X86_64 x86_64
+
 #include <iostream>
 #include <iomanip>
 
