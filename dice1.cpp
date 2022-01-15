a78ca7753d68802cb0f559b4a08365888e234561
wanisha <wanisha@studen.21-school.ru>
2022-01-15 17:05:50 +0300

    it is almost working up to 143 dice with 143 different sides

--- dice1.cpp
+++ dice1.cpp
@@ -3,7 +3,7 @@
 //dice is free software: you can redistribute it and/or modify
 //it under the terms of the GNU General Public License as published by
 //the Free Software Foundation, either version 3 of the License, or
-//(at your option) any later version.
+//any later version.
 //
 //dice is distributed in the hope that it will be useful,
 //but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -14,7 +14,7 @@
 //along with dice.  If not, see <https://www.gnu.org/licenses/>.
 
 //
-// Created by pavlyukevich.anton@gmail.com on 08.01.22
+// Created by pavlyukevich.anton@gmail.com on 15.01.22
 //
 
 // built and tested on Darwin Kernel Version 18.7.0: Tue Jun 22 19:37:08 PDT 2021; root:xnu-4903.278.70~1/RELEASE_X86_64 x86_64
@@ -24,168 +24,156 @@
 #include <complex>
 #include <vector>
 #include <map>
-#include <cmath>
 
-//// calculates sample space
-static void calculate_sample_space(std::vector<float> &options, std::multimap<float, float> &possibilities, float &sample_space, float &number_of_sides)
+#define YELLOW "\033[33m"
+#define B_RED "\033[91m"
+#define DEFAULT "\033[0m"
+
+//calculate permutations
+static void calculate_permutations(std::vector<double> &results_copy, std::multimap<double, double> &permutations, double &sample_space, double &number_of_sides)
 {
-	float i = 0;
-	float k = 0;
-	float j = 0;
-	float options_end = *(options.end() - 1);
-	std::vector<float> options_copy = options;
+	double i = 0;
+	double k = 0;
+	double j = 0;
+	double results_end = *(results_copy.end() - 1);
+	std::vector<double> results_copy_copy = results_copy;
 
-	while (j <= sample_space)
+	while (j < sample_space)
 	{
 		while (k < number_of_sides)
 		{
 			while (i < number_of_sides)
 			{
-				if ((*options_copy.begin() + i) == options_end)
+				if ((*results_copy_copy.begin() + i) == results_end)
 				{
-					possibilities.insert(std::pair<float, float>(*options_copy.begin() + i, *options_copy.begin() + i));
+					permutations.insert(std::pair<double, double>(*results_copy_copy.begin() + i, *results_copy_copy.begin() + i));
 					return ;
 				}
-				possibilities.insert(std::pair<float, float>(*options_copy.begin() + i, *options_copy.begin() + i));
+				permutations.insert(std::pair<double, double>(*results_copy_copy.begin() + i, *results_copy_copy.begin() + i));
 				i++;
 			}
-//			std::cout << std::endl;
 			i = 0;
-			options_copy.erase(options_copy.begin());
+			results_copy_copy.erase(results_copy_copy.begin());
 			k++;
 		}
-		options_copy = options;
-		options_copy.erase(options.begin());
+		results_copy_copy = results_copy;
+		results_copy_copy.erase(results_copy.begin());
 		i = 0;
 		k = 0;
 		j++;
 	}
 }
 
+//check sample_space for infinity
+static void	check_sample_space_for_infinity(double &sample_space)
+{
+	if (std::isinf(sample_space))
+	{
+		std::cout << B_RED << "that is too much even for me" << DEFAULT << std::endl;
+		exit(EXIT_SUCCESS);
+	}
+}
+
+//calculate results
+static void calculate_results(std::vector<double> &results, const double &number_of_dice, const double &number_of_sides)
+{
+	for (double i = 1; i <= (number_of_dice * number_of_sides); i++)
+	{
+		if (number_of_dice == 1)
+			results.push_back(i);
+		else if (i == 1)
+		{
+			i = number_of_dice;
+			results.push_back(i);
+		}
+		else
+			results.push_back(i);
+	}
+}
+
+//convert arguments to double
+static void convert_arguments_to_double(const char **argv, double &number_of_dice, double &number_of_sides)
+{
+	try
+	{
+		number_of_dice = std::stod(argv[1]);
+		number_of_sides = std::stod(argv[2]);
+	}
+	catch (const std::invalid_argument &ex)
+	{
+		std::cout << ex.what() << std::endl;
+	}
+	if ((number_of_dice == 9 && number_of_sides == 5)
+		|| (number_of_dice == 9 && number_of_sides == 9)
+		|| (number_of_dice == 9 && number_of_sides == 10)
+		|| (number_of_dice == 9 && number_of_sides == 11)
+		|| (number_of_dice == 9 && number_of_sides == 12)
+		|| (number_of_dice == 10 && number_of_sides == 8)
+		|| (number_of_dice == 10 && number_of_sides == 9)
+		|| (number_of_dice == 10 && number_of_sides == 10)
+		|| (number_of_dice == 11 && number_of_sides == 9)
+		|| (number_of_dice == 11 && number_of_sides == 10)
+		|| (number_of_dice == 12 && number_of_sides == 9)
+		|| (number_of_dice == 12 && number_of_sides == 12)
+		|| (number_of_dice == 12 && number_of_sides == 13)
+		|| (number_of_dice == 12 && number_of_sides == 14)
+		|| (number_of_dice == 12 && number_of_sides == 15)
+		|| (number_of_dice == 12 && number_of_sides == 16)
+		|| (number_of_dice == 13 && number_of_sides == 12)
+		|| (number_of_dice == 13 && number_of_sides == 13)
+		|| (number_of_dice == 13 && number_of_sides == 14)
+		|| (number_of_dice == 13 && number_of_sides == 15)
+		|| (number_of_dice == 14 && number_of_sides == 12)
+		|| (number_of_dice == 14 && number_of_sides == 13)
+		|| (number_of_dice == 14 && number_of_sides == 14)
+		|| (number_of_dice == 15 && number_of_sides == 12)
+		|| (number_of_dice == 15 && number_of_sides == 13)
+		|| (number_of_dice == 16 && number_of_sides == 12)
+		|| (number_of_dice == 17 && number_of_sides == 12))
+	{
+		std::cout << YELLOW << "for some reason this case does not work" << DEFAULT << std::endl;
+		exit(EXIT_SUCCESS);
+	}
+}
+
+//output probabilities
+static void output_probabilities(std::vector<double> &results, const std::multimap<double, double>	&permutations, double &sample_space)
+{
+	std::vector<double>::iterator it = results.begin();
+
+	std::cout << "Roll a ...\t";
+	std::cout << "Probability" << std::endl;
+	while (it != results.end())
+	{
+		std::cout << std::setw(10) << *it << "\t" << (permutations.count(*it) / sample_space) * 100 << " %" << std::endl;
+		it++;
+	}
+}
+
+//prompt usage
 void prompt_usage()
 {
-	std::cout << "Usage:\n./a.out <number of dice> <number of sides>" << std::endl;
+	std::cout << YELLOW << "Usage:\n./a.out <number of dice> <number of sides>" << DEFAULT << std::endl;
 	exit(EXIT_SUCCESS);
 }
 
-int main(int argc, char **argv)
+int main(const int argc, const char **argv)
 {
 	if (argc == 3)
 	{
-		unsigned int				ex = 0;
-		float						sample_space = 0, number_of_sides = 0, number_of_dice = 0;
-		std::vector<float>			options, options_copy;
-		std::multimap<float, float>	possibilities;
+		double							sample_space = 0, number_of_sides = 0, number_of_dice = 0;
+		std::vector<double>				results, results_copy;
+		std::multimap<double, double>	permutations;
 
-		try
-		{
-			number_of_dice = std::stof(argv[1]);
-			number_of_sides = std::stof(argv[2]);
-		}
-		catch (const std::invalid_argument &exc)
-		{
-			ex++;
-			std::cout << "#" << ex << " " << exc.what() << std::endl;
-		}
-		if (number_of_dice == 3 && number_of_sides == 2)
-		{
-			std::cout << "for some reason I get a segmentation fault in this case" << std::endl;
-			return (EXIT_FAILURE);
-		}
-		else if (number_of_dice == 3 && number_of_sides == 3)
-		{
-			std::cout << "for some reason I abort in this case" << std::endl;
-			return (EXIT_FAILURE);
-		}
-		else if (number_of_dice == 12 && number_of_sides == 12)
-		{
-			std::cout << "for some reason I get a segmentation fault in this case" << std::endl;
-			return (EXIT_FAILURE);
-		}
-		else if (number_of_dice == 11 && number_of_sides == 13)
-		{
-			std::cout << "for some reason I get a segmentation fault in this case" << std::endl;
-			return (EXIT_FAILURE);
-		}
-		else if (number_of_dice == 12 && number_of_sides == 13)
-		{
-			std::cout << "for some reason I get a segmentation fault in this case" << std::endl;
-			return (EXIT_FAILURE);
-		}
-		else if (number_of_dice == 13 && number_of_sides == 13)
-		{
-			std::cout << "for some reason I get a segmentation fault in this case" << std::endl;
-			return (EXIT_FAILURE);
-		}
-		else if (number_of_dice == 13 && number_of_sides == 12)
-		{
-			std::cout << "for some reason I get a segmentation fault in this case" << std::endl;
-			return (EXIT_FAILURE);
-		}
-		else if (number_of_dice == 14 && number_of_sides == 13)
-		{
-			std::cout << "for some reason I get a segmentation fault in this case" << std::endl;
-			return (EXIT_FAILURE);
-		}
-		else if (number_of_dice == 13 && number_of_sides == 14)
-		{
-			std::cout << "for some reason I get a segmentation fault in this case" << std::endl;
-			return (EXIT_FAILURE);
-		}
-		else if (number_of_dice > 16 && number_of_sides > 16)
-		{
-			std::cout << "that is too much even for me" << std::endl;
-			return (EXIT_FAILURE);
-		}
-		else if (number_of_dice == 16 && number_of_sides > 16)
-		{
-			std::cout << "that is too much even for me" << std::endl;
-			return (EXIT_FAILURE);
-		}
-		else if (number_of_dice > 16 && number_of_sides == 16)
-		{
-			std::cout << "that is too much even for me" << std::endl;
-			return (EXIT_FAILURE);
-		}
+		convert_arguments_to_double(argv, number_of_dice, number_of_sides);
 		sample_space = pow(number_of_sides, number_of_dice);
-		if (std::isinf(sample_space))
-		{
-			std::cout << "that is too much even for me" << std::endl;
-			exit(EXIT_SUCCESS);
-		}
-		for (float i = 1; i <= (number_of_sides * number_of_dice); i++)
-		{
-			if (number_of_dice == 1)
-				options.push_back(i);
-			else if (i == 1)
-			{
-				i = number_of_dice;
-				options.push_back(i);
-			}
-			else
-				options.push_back(i);
-		}
-		options_copy = options;
-//		std::cout << "options.size() =\t"<< options_copy.size() << std::endl;
-		for (std::vector<float>::iterator it = options_copy.begin(); it < options_copy.end(); ++it)
-			std::cout << *it << " ";
-		std::cout << std::endl;
-
-		calculate_sample_space(options_copy, possibilities, sample_space, number_of_sides);
-
-		std::vector<float>::iterator it = options.begin();
-		std::cout << "Roll a ...\t";
-		std::cout << "Probability" << std::endl;
-		while (it != options.end())
-		{
-			if (*it == 0)
-				exit(EXIT_FAILURE);
-			std::cout << std::setw(10) << *it << "\t" << (possibilities.count(*it) / sample_space) * 100 << " %" << std::endl;
-			it++;
-		}
+		check_sample_space_for_infinity(sample_space);
+		calculate_results(results, number_of_dice, number_of_sides);
+		results_copy = results;
+		calculate_permutations(results_copy, permutations, sample_space, number_of_sides);
+		output_probabilities(results, permutations, sample_space);
 	}
 	else
 		prompt_usage();
-
 	return (0);
 }
